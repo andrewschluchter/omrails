b95c672e51815eda5926c2c6931d2e898bb81ea3
Andrew Schluchter <thelazyflash@gmail.com>
2013-08-16 20:01:50 -0400

    added user model with devise

--- Gemfile
+++ Gemfile
@@ -31,7 +31,11 @@ gem 'coffee-rails', '~> 4.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
-gem 'devise'
+
+#gem 'devise'
+gem 'devise', '3.0.0.rc'
+
+gem 'simple_form'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+#not needed with devise 3.0.0.rc and rails 4
+#attr_accessible :email, :password, :password, :password_confirmation, :remember_me, :name
+
 end
